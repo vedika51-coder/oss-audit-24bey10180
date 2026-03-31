0d1502d9f0af3c53295c31a71034b86beeaf55da
vedika51-coder <vedika.24bey10180@vitbhopal.ac.in>
2026-03-31 10:54:34 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-if command -v vlc &> /dev/null
-then
-echo "VLC is installed"
-dpkg -s vlc | grep -E "Version|Maintainer|Description"
+# Script 2: FOSS Package Inspector
+
+PACKAGE="vlc"
+
+# Check if VLC is installed
+if dpkg -l | grep -q "^ii  $PACKAGE"; then
+    echo "$PACKAGE is installed."
+    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
 else
-echo "VLC not installed"
+    echo "$PACKAGE is NOT installed."
 fi
+
+# Case statement
+case $PACKAGE in
+    vlc) echo "VLC: a free and open-source multimedia player that can play almost any media format" ;;
+    git) echo "Git: version control system for developers" ;;
+    firefox) echo "Firefox: open-source web browser" ;;
+    apache2) echo "Apache: web server software" ;;
+    *) echo "Unknown software" ;;
+esac
